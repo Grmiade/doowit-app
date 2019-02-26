3e0460e0dc31480ea0f192800abd452f35e54fac
Jérémy Fauvel <jeremy@jeremys-imac.home>
2019-02-26 12:18:41 +0100

    Add tests for Task component

---
+++ src/components/Task.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import faker from 'faker'
+import { fireEvent, render } from 'react-testing-library'
+
+import Task from './Task'
+
+describe('Task component', () => {
+  it('should trigger onCheck when the user clicks on the checkbox label', () => {
+    const handleCheck = jest.fn()
+    const text = faker.lorem.sentence()
+    const { getByText } = render(<Task onCheck={handleCheck}>{text}</Task>)
+
+    const label = getByText(text).getElementsByTagName('label')[0]
+    fireEvent.click(label)
+
+    expect(handleCheck).toBeCalledTimes(1)
+  })
+
+  it('should trigger onCheck when the user clicks on the card', () => {
+    const handleCheck = jest.fn()
+    const text = faker.lorem.sentence()
+    const { getByText } = render(<Task onCheck={handleCheck}>{text}</Task>)
+
+    const card = getByText(text)
+    fireEvent.click(card)
+
+    expect(handleCheck).toBeCalledTimes(1)
+  })
+})
